b5ad2d211298e2e5f4cad60201bb13d393efb8a7
zhoutao <1055294203@qq.com>
2023-09-15 11:03:01 +0800

    feat:Factory.ts

--- dist/dash/FactoryMaker.d.ts
+++ dist/dash/FactoryMaker.d.ts
@@ -4,6 +4,7 @@ declare const FactoryMaker: {
     readonly __factoryMap: {
         [props: string]: FactoryFunction;
     };
+    m: any;
     getClassFactory<T>(classConstructor: BaseConstructor<T>): FactoryFunction;
     merge<T_1>(classConstructor: BaseConstructor<T_1>, context: object, args: any[]): T_1;
 };
--- src/dash/FactoryMaker.ts
+++ src/dash/FactoryMaker.ts
@@ -1,23 +1,23 @@
 import { BaseConstructor } from "../class/BaseConstructor";
 import { FactoryFunction } from "../types/dash/Factory";
 
-const FactoryMaker = (function() {
+const FactoryMaker = (function () {
     class FactoryMaker {
-        readonly __factoryMap:{[props:string]:FactoryFunction};
+        readonly __factoryMap: { [props: string]: FactoryFunction };
         constructor() {
             this.__factoryMap = {};
         }
-
-        getClassFactory<T>(classConstructor:BaseConstructor<T>): FactoryFunction {
+        m
+        getClassFactory<T>(classConstructor: BaseConstructor<T>): FactoryFunction {
             let factory = this.__factoryMap[classConstructor.name];
             let ctx = this;
-            if(!factory) {
+            if (!factory) {
                 // context 为上下文
-                factory = function(context?:object) {
-                    if(!context) context = {};
+                factory = function (context?: object) {
+                    if (!context) context = {};
                     return {
-                        create(...args:any[]) {
-                            return ctx.merge<T>(classConstructor,context,args);
+                        create(...args: any[]) {
+                            return ctx.merge<T>(classConstructor, context, args);
                         }
 
                     }
@@ -25,12 +25,32 @@ const FactoryMaker = (function() {
             }
             return factory
         }
-        merge<T>(classConstructor:BaseConstructor<T>,context:object,args:any[]):T {
-            let extendsionObject = context[classConstructor.name];
-            if(extendsionObject) {
+        merge<T>(classConstructor: BaseConstructor<T>, context: object, args: any[]): T {
+            let extensionObject = context[classConstructor.name];
+            if (extensionObject) {
+                // 如果获取到的上下文的属性classConstructor.name上有overide属性，意味着需要重写classConstructor上对应的属性
+                if (extensionObject.override) {
+                    let instance = new classConstructor({ context }, ...args);
+                    let override = extensionObject.instance({
+                        context,
+                        parent: instance
+                    })
+                    for (let props in override) {
+                        if (instance.hasOwnProperty(props)) {
+                            instance[props] = parent[props];
+                        }
+                    }
+                } else {
+                    // 如果不需要覆写，则意味着直接拿context中传入的构造函数来替换这个构造函数
+                    return new extensionObject.instance({
+                        context,
+                    })
 
-            }else {
-                return new classConstructor(context,...args);
+                }
+
+
+            } else {
+                return new classConstructor(context, ...args);
             }
         }
 
