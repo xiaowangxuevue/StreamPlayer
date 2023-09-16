9b6dc3689acc1a521021682c8042e79af64ee4aa
zhoutao <1055294203@qq.com>
2023-09-16 22:51:16 +0800

    feat:dash/Event

--- src/dash/net/URLLoader.ts
+++ src/dash/net/URLLoader.ts
@@ -2,11 +2,14 @@ import { FactoryObject } from "../../types/dash/Factory";
 import { URLConfig, XHRConfig } from "../../types/dash/Net";
 import FactoryMaker from "../FactoryMaker";
 import HTTPRequest from "./HTTPRequest";
+import { EventConstants } from "../event/EventConstants";
+import EventBusFactory,{ EventBus }  from "../event/EventBus";
 import XHRLoaderFactory,{XHRLoader} from "./XHRLoader";
 
 class URLLoader {
     private config: FactoryObject = {};
     private xhrLoader:XHRLoader;
+    private eventBus:EventBus
     constructor(ctx:FactoryObject,...args:any[]) {
         this.config = ctx.context;
         this.setup();
@@ -17,16 +20,19 @@ class URLLoader {
 
     setup() {
         this.xhrLoader = XHRLoaderFactory({}).getInstance();
+        this.eventBus = EventBusFactory({}).getInstance()
     }
     // 每调用一次load函数就发送一次请求
     load(config: URLConfig) {
         //一个HTTPRequest对象才对应一个请求
         let request = new HTTPRequest(config);
+        let ctx =this
 
         this._loadManifest({
             request: request,
             success: function(data) {
                 request.getResponseTime = new Date().getTime();
+                ctx.eventBus.trigger(EventConstants.MANIFEST_LOADED,data)
                 console.log(this , data);
             },
             error: function(error) {
