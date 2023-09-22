43765217bd8b6db6b43762aec1389cd59e9da16e
zhoutao <1055294203@qq.com>
2023-09-22 10:38:42 +0800

    feat:添加播放倍数和分辨率

--- dist/component/Controller/controller.d.ts
+++ dist/component/Controller/controller.d.ts
@@ -3,22 +3,25 @@ import "./controller.less";
 export declare class Controller extends BaseEvent {
     private template_;
     private container;
+    private video;
     private videoPlayBtn;
     private currentTime;
     private summaryTime;
-    private video;
     private fullScreen;
     private volumeBtn;
     private volumeSet;
     private volumeDot;
     private volumeProgress;
+    private volumeCompleted;
     private playRate;
+    private playRateSet;
     private resolvePower;
+    private resolvePowerSet;
     private settings;
     constructor(container: HTMLElement);
     get template(): HTMLElement | string;
     init(): void;
     initControllerEvent(): void;
     initEvent(): void;
-    handleMouseMove(e: MouseEvent): void;
+    handleMouseMove(e: MouseEvent, type: "volume" | "playrate" | "resolvepower"): void;
 }
--- dist/player.es.js
+++ dist/player.es.js
@@ -1827,46 +1827,64 @@ class Controller extends BaseEvent {
     }
     init() {
         this.template_ = `
-        <div class="${styles["video-play"]}">
-            <div class="${styles["video-subplay"]}">
-                <div class="${styles["video-start-pause"]}">
-                    <i class="${icon["iconfont"]} ${icon["icon-bofang"]}"></i>
-                </div>
-                <div class="${styles["video-duration"]}">
-                    <span class="${styles["video-duration-completed"]}">00:00</span>&nbsp;/&nbsp;<span class="${styles["video-duration-all"]}">00:00</span>
-                </div>
-                <div class="${styles["video-settings"]}">
-                <div class="${styles["video-resolvepower"]} ${styles["video-controller"]}" aria-label="分辨率">
-                    分辨率
-                </div>
-                <div class="${styles["video-playrate"]} ${styles["video-controller"]}" aria-label="倍速">
-                    倍速
-                </div>
-                <div class="${styles["video-volume"]} ${styles["video-controller"]}" aria-label="音量">
-                <div class="${styles["video-volume-set"]}" aria-label="调节音量" style="display:none; bottom:41px" >
-                <div class="${styles["video-volume-show"]}">48</div>
-                <div class="${styles["video-volume-progress"]}">
-                  <div class="${styles["video-volume-completed"]}"></div>
-                  <div class="${styles["video-volume-dot"]}"></div>
-                </div>
+          <div class="${styles["video-play"]}">
+              <div class="${styles["video-subplay"]}">
+                  <div class="${styles["video-start-pause"]}">
+                      <i class="${icon["iconfont"]} ${icon["icon-bofang"]}"></i>
+                  </div>
+                  <div class="${styles["video-duration"]}">
+                      <span class="${styles["video-duration-completed"]}">00:00</span>&nbsp;/&nbsp;<span class="${styles["video-duration-all"]}">00:00</span>
+                  </div>
+             </div>
+              <div class="${styles["video-settings"]}">
+                  <div class="${styles["video-resolvepower"]} ${styles["video-controller"]}" aria-label="分辨率">
+                      分辨率
+                      <ul class="${styles["video-resolvepower-set"]}" style="display:none;bottom:41px">
+                        <li><span>1080p超清</span></li>
+                        <li><span>720p高清</span></li>
+                        <li><span>480p标清</span></li>
+                        <li><span>360p流畅</span></li>
+                        <li><span>自动</span></li>
+                      </ul>
+                  </div>
+                  <div class="${styles["video-playrate"]} ${styles["video-controller"]}" aria-label="倍速">
+                      倍速
+                      <ul class="${styles["video-playrate-set"]}" aria-label="调节播放速度" style="display:none; bottom:41px">
+                        <li>2.0x</li>
+                        <li>1.5x</li>
+                        <li>1.25x</li>
+                        <li>1.0x</li>
+                        <li>0.75x</li>
+                        <li>0.5x</li>
+                      </ul>
+                  </div>
+                  <div class="${styles["video-volume"]} ${styles["video-controller"]}" aria-label="音量">
+                      <div class="${styles["video-volume-set"]}" aria-label="调节音量" style="display:none; bottom:41px" >
+                        <div class="${styles["video-volume-show"]}">48</div>
+                        <div class="${styles["video-volume-progress"]}" style="height: 70px">
+                          <div class="${styles["video-volume-completed"]}" style="height: 0"></div>
+                          <div class="${styles["video-volume-dot"]}" style="bottom: 100%"></div>
+                        </div>
+                      </div>
+                      ${volumeSVG}
+                  </div>
+                  <div class="${styles["video-subsettings"]} ${styles["video-controller"]}" aria-label="设置">
+                      ${settingSVG}
+                  </div>
+                  <div class="${styles["video-fullscreen"]} ${styles["video-controller"]}" aria-label="全屏">
+                      ${fullScreenSVG}
+                  </div>
               </div>
-                    ${volumeSVG}
-                </div>
-                <div class="${styles["video-subsettings"]} ${styles["video-controller"]}" aria-label="设置">
-                    ${settingSVG}
-                </div>
-                <div class="${styles["video-fullscreen"]} ${styles["video-controller"]}" aria-label="全屏">
-                  
-                ${fullScreenSVG}
-            </div>
-        </div>
-    </div>
-    `;
+          </div>
+      `;
     }
-    /*
-    @description 监听鼠标点击事件来控制是否暂停还是播放
-    */
     initControllerEvent() {
+        this.volumeCompleted.style.height = this.video.volume * 100 + "%";
+        this.volumeDot.style.bottom =
+            parseInt(this.volumeProgress.style.height) * this.video.volume - 6 + "px";
+        /**
+         * @description 监听鼠标的点击事件来决定是否暂停还是播放视频
+         */
         this.videoPlayBtn.onclick = (e) => {
             if (this.video.paused) {
                 this.video.play();
@@ -1875,7 +1893,9 @@ class Controller extends BaseEvent {
                 this.video.pause();
             }
         };
-        // 全屏
+        /**
+         * @description 点击进入全屏模式
+         */
         this.fullScreen.onclick = () => {
             if (this.container.requestFullscreen && !document.fullscreenElement) {
                 this.container.requestFullscreen(); //该函数请求全屏
@@ -1885,15 +1905,60 @@ class Controller extends BaseEvent {
             }
         };
         /**
- * @desciption 显示音量的设置
- * TODO:这部分控制选项的显示和隐藏的逻辑可以复用
- */
+         * @desciption 显示音量的设置
+         * TODO:这部分控制选项的显示和隐藏的逻辑可以复用
+         */
         this.volumeBtn.onmouseenter = (e) => {
             this.volumeSet.style.display = "block";
+            let ctx = this;
             document.body.onmousemove = (e) => {
-                let ctx = this;
-                document.addEventListener("mousemove", this.handleMouseMove.bind(ctx));
+                ctx.handleMouseMove(e, "volume");
             };
+        };
+        this.playRate.onmouseenter = (e) => {
+            this.playRateSet.style.display = "block";
+            let ctx = this;
+            document.body.onmousemove = (e) => {
+                ctx.handleMouseMove(e, "playrate");
+            };
+        };
+        this.resolvePower.onmouseenter = (e) => {
+            this.resolvePowerSet.style.display = "block";
+            let ctx = this;
+            document.body.onmousemove = (e) => {
+                ctx.handleMouseMove(e, "resolvepower");
+            };
+        };
+        // this.volumeProgress.onclick = (e:MouseEvent) => {
+        //   console.log(e.offsetY,this.volumeProgress.clientHeight)
+        //   let offsetY = this.volumeProgress.clientHeight - e.offsetY;
+        //   let scale = offsetY / this.volumeProgress.clientHeight;
+        //   if (scale < 0) {
+        //     scale = 0;
+        //   } else if (scale > 1) {
+        //     scale = 1;
+        //   }
+        //   this.volumeCompleted.style.height = scale * 100 + "%"
+        //   this.volumeDot.style.bottom = this.volumeProgress.clientHeight * scale - 6 + "px";
+        // }
+        this.volumeDot.onmousedown = (e) => {
+            let mouseY = e.pageY;
+            let comHeight = this.volumeCompleted.clientHeight;
+            document.body.onmousemove = (e) => {
+                let pageY = e.pageY;
+                let scale = (mouseY - pageY + comHeight) / this.volumeProgress.clientHeight;
+                if (scale > 1)
+                    scale = 1;
+                else if (scale < 0)
+                    scale = 0;
+                this.volumeCompleted.style.height = scale * 100 + "%";
+                this.volumeDot.style.bottom = this.volumeProgress.clientHeight * scale - 6 + "px";
+                this.video.volume = scale;
+            };
+            document.body.onmouseup = () => {
+                document.body.onmousemove = null;
+            };
+            e.preventDefault();
         };
     }
     initEvent() {
@@ -1909,6 +1974,9 @@ class Controller extends BaseEvent {
         this.on("timeupdate", (current) => {
             this.currentTime.innerHTML = formatTime(current);
         });
+        /**
+         * @description 模板字符已经渲染到页面上，可以获取DOM元素了
+         */
         this.on("mounted", () => {
             this.videoPlayBtn = this.container.querySelector(`.${styles["video-start-pause"]} i`);
             this.currentTime = this.container.querySelector(`.${styles["video-duration-completed"]}`);
@@ -1916,19 +1984,38 @@ class Controller extends BaseEvent {
             this.video = this.container.querySelector("video");
             this.fullScreen = this.container.querySelector(`.${styles["video-fullscreen"]}`);
             this.volumeBtn = this.container.querySelector(`.${styles["video-volume"]}`);
-            console.log(this.volumeBtn, this.volumeBtn.style, 'btn');
             this.volumeSet = this.container.querySelector(`.${styles["video-volume-set"]}`);
+            this.volumeCompleted = this.container.querySelector(`.${styles["video-volume-completed"]}`);
+            this.volumeProgress = this.container.querySelector(`.${styles["video-volume-progress"]}`);
+            this.volumeDot = this.container.querySelector(`.${styles["video-volume-dot"]}`);
+            this.playRate = this.container.querySelector(`.${styles["video-playrate"]}`);
+            this.playRateSet = this.container.querySelector(`.${styles["video-playrate-set"]}`);
+            this.resolvePower = this.container.querySelector(`.${styles["video-resolvepower"]}`);
+            this.resolvePowerSet = this.container.querySelector(`.${styles["video-resolvepower-set"]}`);
             this.initControllerEvent();
         });
     }
-    handleMouseMove(e) {
+    handleMouseMove(e, type) {
         let pX = e.pageX, pY = e.pageY;
-        console.log(pX, 'pxxx');
         let ctx = this;
-        if (!checkIsMouseInRange(this.volumeBtn, this.volumeSet, pX, pY)) {
-            this.volumeSet.style.display = "none";
+        if (type === "volume") {
+            if (!checkIsMouseInRange(ctx.volumeBtn, ctx.volumeSet, pX, pY)) {
+                ctx.volumeSet.style.display = "none";
+                document.body.onmousemove = null;
+            }
         }
-        document.removeEventListener("mousemove", ctx.handleMouseMove);
+        else if (type === "playrate") {
+            if (!checkIsMouseInRange(ctx.playRate, ctx.playRateSet, pX, pY)) {
+                ctx.playRateSet.style.display = "none";
+                document.body.onmousemove = null;
+            }
+        }
+        else if (type === "resolvepower") {
+            if (!checkIsMouseInRange(ctx.resolvePower, ctx.resolvePowerSet, pX, pY)) {
+                ctx.resolvePowerSet.style.display = "none";
+                document.body.onmousemove = null;
+            }
+        }
     }
 }
 
@@ -2062,12 +2149,14 @@ const styles = {
     "video-controller": "controller_video-controller__MqNia",
     "video-playrate": "controller_video-playrate__lmym3",
     "video-resolvepower": "controller_video-resolvepower__yDRda",
+    "video-resolvepower-set": "controller_video-resolvepower-set__mWs9u",
     "video-volume": "controller_video-volume__6xzJB",
     "video-volume-set": "controller_video-volume-set__MZ1ks",
     "video-volume-show": "controller_video-volume-show__uRgS1",
     "video-volume-progress": "controller_video-voulme-progress__QAOAm",
     "video-volume-completed": "controller_video-volume-completed__R0FaX",
     "video-volume-dot": "controller_video-volume-dot__6sC-V",
+    "video-playrate-set": "controller_video-playrate-set__FH52z",
     "loading-mask": "",
     "loading-container": "",
     "loading-item": "",
